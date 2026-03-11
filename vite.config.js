9ba348ffd52d4cb2249a2ba6d41898223fad5987
anshulyadavv <anshulyadav876@gmail.com>
2026-03-12 00:10:32 +0530

    workflows github

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+export default defineConfig({
+  plugins: [react()],
+  base: "beacon",
+})
+```
+
+After pushing, GitHub Actions will build and deploy automatically. Your site will be live at:
+```
+https://anshulyadavv.github.io/beacon/
